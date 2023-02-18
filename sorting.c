5437e88cd2c450df8ed37a8b9afcc58147111cb6
Walid Araissi <waraissi@e2r6p7.1337.ma>
2023-02-18 16:53:33 +0100

    done 2 and 5

--- sorting.c
+++ sorting.c
@@ -6,7 +6,7 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 17:27:37 by waraissi          #+#    #+#             */
-/*   Updated: 2023/02/17 22:27:23 by waraissi         ###   ########.fr       */
+/*   Updated: 2023/02/18 16:38:23 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ int    lar_elem(t_list *vars)
 {
     int lrg;
 
-    lrg = 0;
+    lrg = vars->content;
     while (vars->next)
     {
-        if (vars->content < vars->next->content)
+        if (vars->content < vars->next->content && lrg < vars->next->content)
             lrg = vars->next->content;
         vars = vars->next;
     }
@@ -30,33 +30,41 @@ int    low_elem(t_list *vars)
 {
     int min;
 
-    min = 0;
+    min = vars->content;
     while (vars->next)
     {
-        if (vars->content > vars->next->content)
+        if (vars->content > vars->next->content && min > vars->next->content)
             min = vars->next->content;
         vars = vars->next;
     }
     return (min);
         
 }
+void    sort_more_five(t_vars *vars)
+{
+    (void)vars;
+}
+
 void    sort_three(t_vars *vars)
 {   
     t_list  *head;
-    t_list  *lst;
 
     head = vars->stack_a;
-    lst = ft_lstlast(vars->stack_a);
-    if (head->next->next && head->content > head->next->content
-        && head->next->content > head->next->next->content)
-        ra(&vars->stack_a);
-    else if (head->content == lar_elem(head))
-        ra(&vars->stack_a);
-    else if (lst->content == low_elem(head))
-        rra(&vars->stack_a);
-    head = vars->stack_a;
-    if (head->content > head->next->content)
+    if (head->content > head->next->content
+            && head->next->content < head->next->next->content)
         sa(&vars->stack_a);
+    else if (head->content > head->next->content
+                && head->next->content > head->next->next->content)
+    {
+        sa(&vars->stack_a);
+        rra(&vars->stack_a);
+    }
+    else if (head->content < head->next->content
+                && head->next->content > head->next->next->content)
+    {
+        sa(&vars->stack_a);
+        ra(&vars->stack_a);
+    }
 }
 
 int     min_pos(t_list  *vars)
@@ -82,46 +90,40 @@ void    sort_till_five(t_vars *vars)
 {
     t_list  *head;
     int size;
+    int i;
 
+    i = 3;
     head = vars->stack_a;
-    size = ft_lstsize(head) - 1;
-    
-    if (min_pos(head) > size / 2)
+    size = ft_lstsize(head);
+    while (i < size)
     {
-        if (min_pos(head) == size)
-            rra(&vars->stack_a);
-        if (min_pos(head) == size - 1)
+        if (min_pos(head) > ft_lstsize(head) / 2)
         {
-            ra(&vars->stack_a);
-            ra(&vars->stack_a);
+            if (min_pos(head) == ft_lstsize(head) - 1)
+                ra(&vars->stack_a);
+            else if (min_pos(head) == ft_lstsize(head) - 2)
+            {
+                rra(&vars->stack_a);
+                rra(&vars->stack_a);
+            }
         }
-    }
-    else if (min_pos(head) <= size / 2)
-    {
-        if (min_pos(head) == size / 2)
+        else if (min_pos(head) <= ft_lstsize(head) / 2)
         {
-            ra(&vars->stack_a);
-            ra(&vars->stack_a);
+            if (min_pos(head) == ft_lstsize(head) / 2)
+            {
+                ra(&vars->stack_a);
+                ra(&vars->stack_a);
+            }
+            else if (min_pos(head) == (ft_lstsize(head) / 2) - 1)
+                ra(&vars->stack_a);
         }
+        pb(&vars->stack_a,&vars->stack_b);
+        head = vars->stack_a;
+        i++;
     }
-    pb(&vars->stack_a,&vars->stack_b);
     sort_three(vars);
-    // pb(&vars->stack_a,&vars->stack_b);
-    // sort_three(vars);
-    // pa(&vars->stack_a,&vars->stack_b);
-    // pa(&vars->stack_a,&vars->stack_b);
-    printf("-----------stack_a-------------\n");
-    while (vars->stack_a)
-    {
-        printf("%d\n",vars->stack_a->content);
-        vars->stack_a = vars->stack_a->next;
-    }
-    printf("-----------stack_b-------------\n");
-    while (vars->stack_b)
-    {
-        printf("%d\n",vars->stack_b->content);
-        vars->stack_b = vars->stack_b->next;
-    }
+    pa(&vars->stack_a,&vars->stack_b);
+    pa(&vars->stack_a,&vars->stack_b);
 }
 
 void    ft_sorting(t_vars *vars)
@@ -131,6 +133,8 @@ void    ft_sorting(t_vars *vars)
     l = ft_lstsize(vars->stack_a);
     if (l <= 3)
         sort_three(vars);
-    if (l == 4 || l ==5)
+    else if (l == 4 || l ==5)
         sort_till_five(vars);
+    else if (l > 5)
+        sort_more_five(vars);
 }
